f4eee30926deb2d7480c857c09ce669d3a1fa584
CodyMKW <CodyMKW@users.noreply.github.com>
2024-11-04 04:51:22 +0000

    fix

--- playlists/script.js
+++ playlists/script.js
@@ -34,8 +34,8 @@ function createPlaylistCard(playlist) {
         card.classList.add('official-playlist');
     }
     // Add a special class for official Nintendo Music Credit Project playlists
-    if (playlist.creator.toLowerCase() === 'nintendo-music-credit-project') {
-        card.classList.add('nmcredit-playlist');
+    if (playlist.creator.trim().toLowerCase() === 'nintendo music credit project') {
+    card.classList.add('nmcredit-playlist');
     }
     card.onclick = () => window.open(playlist.link, '_blank');
 
