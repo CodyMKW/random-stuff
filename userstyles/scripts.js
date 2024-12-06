0c94f5b5665b08f78e7804c4642fc0430e4b10b0
CodyMKW <CodyMKW@users.noreply.github.com>
2024-12-06 23:28:59 +0000

    changes

--- userstyles/scripts.js
+++ userstyles/scripts.js
@@ -17,7 +17,7 @@ fetch('https://api.npoint.io/a7fc8fd835b5690622fd')
         <img src="${style.thumbnail}" alt="${style.name}">
         <h3>${style.name}</h3>
         <p>Created by: ${style.creator}</p>
-        <button onclick="window.open('${style.installLink}', '_blank')">Install</button>
+        <button onclick="window.open('${style.installLink}', '_blank')"> <span>Install</span> </button>
       `;
 
       container.appendChild(card);
